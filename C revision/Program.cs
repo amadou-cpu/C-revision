0be10bdb4f503b1d63c0374160f7676692e1b2a6
cheik pc <cheik pc@CHEIKPC-PC>
2022-11-08 13:45:53 +0100

    Ajoutez des fichiers projet.

---
+++ C revision/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace C_revision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //déclarations
+            int âge;
+
+            //affichage de l'âge
+            Console.Write("entre un âge");
+            âge = int.Parse(Console.ReadLine());
+
+            //condition
+            if (âge >= 18)
+            {
+                Console.Write("vous êtes majeur");
+
+
+
+            }
+            else
+            {
+                Console.Write("vous êtes mineur ");
+            }  
+            //l'âge de majorité
+                Console.WriteLine("vous serez majeur dans " + (18 - âge) + " ans");
+                Console.ReadLine();
+            
+}
+        }
+    }
+
